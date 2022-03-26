09666f40aae86bdb23b9f685f42184b43f04f32b
Shubhadarshini <nadar.shubhadarshini18@siesgst.ac.in>
2022-03-27 01:44:36 +0530

    Hierarchy

---
+++ app/src/main/java/com/example/visayatniti/Hierarchy.java
@@ -0,0 +1,14 @@
+package com.example.visayatniti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hierarchy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hierarchy);
+    }
+}
